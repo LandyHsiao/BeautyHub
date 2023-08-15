c3ac45f6cd8f589b3e115ef07a74d4f71ea43045
“ann” <136979138+ann328@users.noreply.github.com>
2023-08-15 20:52:18 +0800

    feat: 🎸 add member_login fix header

--- vite.config.js
+++ vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig({
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: "pages/footer-test.html",
+    open: "pages/member_login.html",
   },
   build: {
     rollupOptions: {
